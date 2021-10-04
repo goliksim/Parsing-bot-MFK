2fab14819b05017bd2a2afaa4ced74a46046f65a
goliksim <66952748+goliksim@users.noreply.github.com>
2021-10-04 03:20:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 import os
 import requests
 import telebot
-#from flask import Flask , request
+from flask import Flask , request
 from bs4 import BeautifulSoup # Модуль для работы с HTML
 import time # Модуль для остановки программы
 import datetime
 
 TOKEN = '2001618640:AAE-bA_qXoY878BTKFj3iNFbMFXS8PfBaEE'
-#APP_URL = f'https://mfkbot.herokuapp.com/{TOKEN}'
+APP_URL = f'https://mfkbot.herokuapp.com/{TOKEN}'
 bot = telebot.TeleBot(TOKEN)
 
-#server = Flask(__name__)
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
@@ -23,19 +23,19 @@ def echo(message):
     bot.reply_to(message, message.text)
 
 
-#@server.route('/' + TOKEN, methods=['POST'])
-#def get_message():
-#    json_string = request.get_data().decode('utf-8')
-#    update = telebot.types.Update.de_json(json_string)
-#    bot.process_new_updates([update])
-#    return '!', 200
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
 
 
-#@server.route('/')
-#def webhook():
-#    bot.remove_webhook()
-#    bot.set_webhook(url=APP_URL)
-#    return '!', 200
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
 
 # Словарь с ссылками на сайты курсов МФК
 Site_Dict = {
@@ -100,7 +100,7 @@ class Free_Places:
 
 if __name__ == '__main__':
 	
-    	#server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    	server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 	
     	SQL = Free_Places('SQL      ')
     	PYTHON = Free_Places('PYTHON   ')
